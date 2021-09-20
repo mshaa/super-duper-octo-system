c174060ed6afa8cba810cffa5275e500a5691ee9
mshaa <winterbred@mail.ru>
2021-09-20 21:19:29 +0700

    init

---
+++ pages/threekit-welcome-iframe.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="button">
+      <button @click="showReactContainer = !showReactContainer">Press me</button>
+    </div>
+    <div v-show="showReactContainer">
+      <iframe id="root" src="/threekit.html" frameborder="0"></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showReactContainer: false
+    }
+  }
+}
+</script>
+
+<style>
+  #root {
+    height: calc(100vh - 50px);
+    width:  100%;
+  }
+
+  .button {
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    padding:  8px;
+  }
+</style>
---
+++ pages/threekit-welcome.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="button">
+      <button @click="showReactContainer = !showReactContainer">Press me</button>
+    </div>
+    <div v-show="showReactContainer">
+      <div id="root"></div>
+      <script src="welcome_threekit-embed.js"></script>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showReactContainer: false
+    }
+  }
+}
+</script>
+
+<style>
+  #root {
+    height: calc(100vh - 50px);
+  }
+
+  .button {
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    padding:  8px;
+  }
+</style>
